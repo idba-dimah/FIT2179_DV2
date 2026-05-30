d1ce7aee25a1ffb2a7a638326ab28ac61e3321b5
idba-dimah <hmoh0045@student.monash.edu>
2026-05-30 14:09:08 +0800

    ADD digital access 2025 heatmap VIZ

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 var Internet2025 = "js/internetAccess_2025.json";
-
 vegaEmbed("#InternetAccess_2025", Internet2025).then(function(result) {
+}).catch(console.error);
 
+var Digital2025 = "js/DigitalAccess_2025.json";
+vegaEmbed("#DigitalAccess_2025", Digital2025).then(function(result) {
 }).catch(console.error);
