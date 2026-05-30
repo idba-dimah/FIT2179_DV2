29e04bac9224293e4a2610cc092ac88ebc30409e
idba-dimah <hmoh0045@student.monash.edu>
2026-05-30 17:31:26 +0800

    ADD internet MHDI scatterplot 2024

--- js/main.js
+++ js/main.js
@@ -5,3 +5,7 @@ vegaEmbed("#InternetAccess_2025", Internet2025).then(function(result) {
 var Digital2025 = "js/DigitalAccess_2025.json";
 vegaEmbed("#DigitalAccess_2025", Digital2025).then(function(result) {
 }).catch(console.error);
+
+var InternetMHDI2024 = "js/Internet_MHDI_2024.json";
+vegaEmbed("#Internet_MHDI_2024", InternetMHDI2024).then(function(result) {
+}).catch(console.error);
