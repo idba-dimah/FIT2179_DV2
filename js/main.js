d2f6435ee986d7d9e799f73ba1a2cbcb721d1b3a
idba-dimah <hmoh0045@student.monash.edu>
2026-06-01 20:33:34 +0800

    DV2 finalization

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var Internet2025 = "js/internetAccess_2025.json";
+var Internet2025 = "js/InternetAccess_2025.json";
 vegaEmbed("#InternetAccess_2025", Internet2025).then(function(result) {
 }).catch(console.error);
 
